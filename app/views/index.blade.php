5f9ca10e94c7bc2073e3fe6d9ed360cf7a820038
Ricardo Gomes <ricardogomes@ricardos-mbp.lan>
2014-01-18 21:51:12 +0000

    Added CodeSleeve AssetPipeLine, and moved assets

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,13 +1,22 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Byte&Code's Laravel Starter Site</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<?= stylesheet_link_tag() ?>
 </head>
 <body>
-	
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
+	<section>
+		<div class="container" style="margin-top:100px;">
+			<div class="jumbotron">
+        		<h1>Byte&Code's Laravel Starter Site</h1>
+        		<p class="lead">Welcome to Byte&Code's Laravel Starter Site</p>
+        		<p>This starter site was developed to provider a better starting point to working on Laravel Projects.</p>
+        		<p>Hope you enjoy our work!! And feel free to contact us at <a href="//www.bytencode.com">Byte&Code's Site.</a></p>
+      		</div>
+		</div>
+	</section>
+	<?= javascript_include_tag() ?>
 </body>
 </html>
