ae995419501f497a41dc5c71121ddcf38326b9b3
Ricardo Gomes <ricardogomes@ricardos-mbp.lan>
2014-01-18 18:34:05 +0000

    Added jquery and bootstrap

---
+++ app/views/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Byte&Code's Laravel Starter Site</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+	
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
